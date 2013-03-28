a57c4d1d6a92d4138401a574406970f3a7c7f6dc
jefim <jefim@localhost>
2013-03-28 08:02:07 +0200

    Added notify icon with two context options

--- Wooster/Wooster/MainWindow.xaml.cs
+++ Wooster/Wooster/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -8,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using wf = System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -15,6 +17,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using Wooster.Classes;
+using System.Reflection;
 
 namespace Wooster
 {
@@ -24,6 +27,7 @@ namespace Wooster
     public partial class MainWindow : Window
     {
         private MainWindowViewModel _mainWindowViewModel;
+        private wf.NotifyIcon _notifyIcon;
 
         public MainWindow()
         {
@@ -34,6 +38,51 @@ namespace Wooster
 
             this.IsVisibleChanged += MainWindow_IsVisibleChanged;
             this.Deactivated += MainWindow_Deactivated;
+
+            this.LoadNotificationIcon();
+        }
+
+        private void LoadNotificationIcon()
+        {
+            var asm = typeof(MainWindow).Assembly;
+            var resources = asm.GetManifestResourceNames();
+            var icon = resources.FirstOrDefault(o => o.EndsWith("mug.ico"));
+            using (var stream = asm.GetManifestResourceStream(icon))
+            {
+                this._notifyIcon = new wf.NotifyIcon();
+                this._notifyIcon.Icon = new Icon(stream);
+
+                var settingsStripItem = new wf.ToolStripButton("Settings", null, NotifyIcon_ExitMenuItemClick);
+                var exitStripItem = new wf.ToolStripButton("Exit Wooster", null, NotifyIcon_ExitMenuItemClick);
+                var contextMenuStrip = new wf.ContextMenuStrip();
+
+                contextMenuStrip.ShowImageMargin = false;
+                contextMenuStrip.Items.Add(settingsStripItem);
+                contextMenuStrip.Items.Add(exitStripItem);
+                this._notifyIcon.ContextMenuStrip = contextMenuStrip;
+
+                this._notifyIcon.Visible = true;
+                this._notifyIcon.MouseClick += NotifyIcon_MouseClick;
+            }
+        }
+
+        void NotifyIcon_ExitMenuItemClick(object sender, EventArgs e)
+        {
+            this._notifyIcon.Visible = false;
+            Application.Current.Shutdown();
+        }
+
+        void NotifyIcon_SettingsMenuItemClick(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        void NotifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == wf.MouseButtons.Left)
+            {
+                this.Show();
+            }
         }
 
         void MainWindowViewModel_HideRequest(object sender, EventArgs e)
